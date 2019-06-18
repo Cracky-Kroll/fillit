76cafda13ec0fe9361d3872a93f59f0ab99f4685
Cracky-Kroll <ccarole@student.42.fr>
2019-06-18 19:10:57 +0200

    back

--- backtrack.c
+++ backtrack.c
@@ -6,7 +6,7 @@
 /*   By: ccarole <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 21:36:17 by ccarole           #+#    #+#             */
-/*   Updated: 2019/06/18 18:36:51 by ccarole          ###   ########.fr       */
+/*   Updated: 2019/06/18 19:05:42 by ccarole          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,19 +55,48 @@ char	**new_map(char ***tab, char **map)
 		y++;
 	}
 	return (map);
+}
 
+void		remove_piece(char **map, int i)
+{
+	int		x;
+	int		y;
+
+	x = 0;
+	y = 0;
+	while (map)
+	{
+		x = 0;
+		while (map[y][x] != (i + 65) && map[y][x] != '\0')
+			x++;
+		if (map[y][x] == (i + 65))
+		{
+			map[y][x] = '.';
+			x++;
+		}
+		if (map[y][x] == '\0')
+			y++;
+	}
 }
 
 int			check_map(char ***tab)
 {
+	char	**map;    // pr map
+	int		x;
+	int		y;
+	int		i;        //pour tab - trouver ou initialiser variables 
+	int		r;
+	int		c;
 
-	if (map[y][x] != '.')
-		x++;
+//	if (map[y][x] != '.')
+//		x++;
 	if (map[y][x] == '.')
 	{
 		while (tab[i][r][c] == '.')
 			c++;
-		map[y][x] = i + 65;
+		map[y][x] = i + 65;                      // en recursive pour la suite
+		if (tab[i][r][c] != '.' && tab[i][r][c] != '\0' && map[y][x] == '.')
+				map[y][x] = i + 65;
 		if (tab[i][r][c + 1] != '.' && tab[i][r][c + 1] != '\0' && map[y][x + 1] == '.')
 				map[y][x + 1] = i + 65;
 		if (tab[i][r][c + 2] != '.' && tab[i][r][c + 2] != '\0' && map[y][x + 2] == '.')
@@ -75,10 +104,12 @@ int			check_map(char ***tab)
 		if (tab[i][r][c + 3] != '.' && tab[i][r][c + 3] != '\0' && map[y][x + 3] == '.')
 				map[y][x + 3] = i + 65;
 		if (tab[i][r][c + 4] != '.' && tab[i][r][c + 4] != '\0' && map[y][x + 4] == '.')
-				map[y][x + 4] = i + 65;
+				map[y][x + 4] = i + 65;          // oui, check a droite, si non check desous.- si oui check a droite -> si oui encore a droite / si non a gauche / si moindre erreurs on remplace tous les i + 65 de la map par des points et on check a partir de x + 1 / si pb a partir de  y + 1 / 
 		if (tab[i][r][c + 1] != '.' && tab[i][r][c] != '\0' && (map[y][x + 1] == '\0' || map[y][x + 1] != '.'))
-				map[y][x] == '.';
-		}
+				map[y][x] == '.';             // pareil qu'au dessus 
+
+	}
+
 
 
 }

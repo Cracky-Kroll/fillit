819bf023f5e55963fbbebd79371502afb1b9c194
Cracky-Kroll <ccarole@student.42.fr>
2019-06-19 15:49:43 +0200

    back_en cours

--- backtrack.c
+++ backtrack.c
@@ -6,7 +6,7 @@
 /*   By: ccarole <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 21:36:17 by ccarole           #+#    #+#             */
-/*   Updated: 2019/06/18 19:05:42 by ccarole          ###   ########.fr       */
+/*   Updated: 2019/06/19 15:49:14 by ccarole          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,11 @@
 
 int	count_tetris(char ***tab);
 
-char	**malloc_map(char ***tab)
+char	**malloc_map(char ***tab, int nb_p)
 {
 	int		size;
 	char	**map;
 	int		i;
-	int		nb_p;
 
 	nb_p = count_tetris(tab);
 	size = ft_sqrt(nb_p * 4);
@@ -36,14 +35,16 @@ char	**malloc_map(char ***tab)
 	return (map);
 }
 
-char	**new_map(char ***tab, char **map)
+char		**new_map(char ***tab, int nb_p)
 {
-	int	x;
-	int	y;
+	char	**map;
+	int		x;
+	int		y;
 
+	nb_p = count_tetris(tab);
 	x = 0;
 	y = 0;
-	map = malloc_map(tab);
+	map = malloc_map(tab, nb_p);
 	while (map)
 	{
 		x = 0;
@@ -87,28 +88,70 @@ int			check_map(char ***tab)
 	int		i;        //pour tab - trouver ou initialiser variables 
 	int		r;
 	int		c;
+	int		w;
 
-//	if (map[y][x] != '.')
-//		x++;
-	if (map[y][x] == '.')
+	w = count_tetris(tab);
+	//	if (map[y][x] != '.')
+	//		x++;
+	while (w > 0)
 	{
+		while (map[y][x] != '.' && map[x][y] != '\0')
+		{
+			x++;
+			if (map[x][y] == '\0' && map[y + 1][0] != '\0')
+			{
+				x = 0;
+				y = y + 1;
+		}
+
+			x = x_start;
+			y = y_start;
 		while (tab[i][r][c] == '.')
 			c++;
-		map[y][x] = i + 65;                      // en recursive pour la suite
 		if (tab[i][r][c] != '.' && tab[i][r][c] != '\0' && map[y][x] == '.')
-				map[y][x] = i + 65;
-		if (tab[i][r][c + 1] != '.' && tab[i][r][c + 1] != '\0' && map[y][x + 1] == '.')
-				map[y][x + 1] = i + 65;
-		if (tab[i][r][c + 2] != '.' && tab[i][r][c + 2] != '\0' && map[y][x + 2] == '.')
-				map[y][x + 2] = i + 65;
-		if (tab[i][r][c + 3] != '.' && tab[i][r][c + 3] != '\0' && map[y][x + 3] == '.')
-				map[y][x + 3] = i + 65;
-		if (tab[i][r][c + 4] != '.' && tab[i][r][c + 4] != '\0' && map[y][x + 4] == '.')
-				map[y][x + 4] = i + 65;          // oui, check a droite, si non check desous.- si oui check a droite -> si oui encore a droite / si non a gauche / si moindre erreurs on remplace tous les i + 65 de la map par des points et on check a partir de x + 1 / si pb a partir de  y + 1 / 
-		if (tab[i][r][c + 1] != '.' && tab[i][r][c] != '\0' && (map[y][x + 1] == '\0' || map[y][x + 1] != '.'))
-				map[y][x] == '.';             // pareil qu'au dessus 
+		{
+			map[y][x] = i + 65;
+			c++;
+			x++;
+		}
+		if (tab[i][r][c] == '\0' && r < 3)
+		{
+			r++;
+			x = x - c;
+			c = 0;
+			y++;
+		}
+		if (r == 3 && c == 4)
+		{
+			w--;
+			i++;
+		}
+		if ((tab[i][r][c] != '.' && tab[i][r][c] != '\0') && (map[y][x] == '\0' || map[y][x] != '.'))
+		{
+			remove_piece(map, i);
+			if (map[y][x_start + 1] != '\0')
+				x_start = x_start + 1
+			else if (map[y][x_start + 1] == '\0' && map[y_start + 1][0] != '\0')
+			{
+				x_start = 0;
+				y_start = y_start + 1
+			}
+			if (map[y][x_start + 1] == '\0' && map[y_start + 1][0] == '\0')
+			{
+				free(map);
+				map = new_map(tab, count_tetris(tab) + 1);
+			}
 
+		//		if (tab[i][r][c + 1] != '.' && tab[i][r][c + 1] != '\0' && map[y][x + 1] == '.')
+		//				map[y][x + 1] = i + 65;
+		//		if (tab[i][r][c + 2] != '.' && tab[i][r][c + 2] != '\0' && map[y][x + 2] == '.')
+		//				map[y][x + 2] = i + 65;
+		//		if (tab[i][r][c + 3] != '.' && tab[i][r][c + 3] != '\0' && map[y][x + 3] == '.')
+		//				map[y][x + 3] = i + 65;
+		//		if (tab[i][r][c + 4] != '.' && tab[i][r][c + 4] != '\0' && map[y][x + 4] == '.')
+		//				map[y][x + 4] = i + 65;          // oui, check a droite, si non check desous.- si oui check a droite -> si oui encore a droite / si non a gauche / si moindre erreurs on remplace tous les i + 65 de la map par des points et on check a partir de x + 1 / si pb a partir de  y + 1 / 
 	}
+}
 
 
 
@@ -133,8 +176,8 @@ char		**backtrack(char ***tab)
 	int		nb_p;			// --->> mettre en structure
 	int		round;           // compte nb de fois ou doit agrandir la map
 	int		stop;            //fn s arrete quand toutes les pueces sont posees avec bonne condition
-	
-	
+
+
 	nb_p = count_tetris(tab) + round;    //au debut round = 0, pr pv faire en recursive
 
 }

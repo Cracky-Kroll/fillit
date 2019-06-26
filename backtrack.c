85a078141a46b7ed1b7c7ccf16011c529977cc48
Cracky-Kroll <ccarole@student.42.fr>
2019-06-26 20:29:54 +0200

    backtrack

--- backtrack.c
+++ backtrack.c
@@ -6,7 +6,7 @@
 /*   By: ccarole <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 21:36:17 by ccarole           #+#    #+#             */
-/*   Updated: 2019/06/20 17:58:23 by ccarole          ###   ########.fr       */
+/*   Updated: 2019/06/26 20:28:29 by ccarole          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,38 +31,35 @@ int			ft_approx_sqrt(int nb)
 
 char	**malloc_map(char ***tab, int nb_p)
 {
-	int		size;
-	char	**map;
-	int		i;
-	int		j;
+	t_map	m;
 
 	nb_p = count_tetris(tab);
-	size = ft_approx_sqrt(nb_p * 4);
-	i = 0;
-	j = 0;
-	printf("test_malloc_map_00, i = %d, size = %d, nb tetri = %d\n", i, size, nb_p);
-	if (!(map = (char**)malloc(sizeof (char*) * size + 1)))
+	m.size = ft_approx_sqrt(nb_p * 4);
+	m.y = 0;
+	m.x = 0;
+	printf("test_malloc_map_00, m.y = %d, size = %d, nb tetri = %d\n", m.y, m.size, nb_p);
+	if (!(m.map = (char**)malloc(sizeof (char*) * m.size + 1)))
 		return (NULL);
-	printf("test_malloc_map, i = %d, size = %d, nb tetri = %d\n", i, size, nb_p);
+	printf("test_malloc_map, m.y = %d, size = %d, nb tetri = %d\n", m.y, m.size, nb_p);
 	fflush(stdout);
-	while (i < size)
+	while (m.y < m.size)
 	{
-		if (!(map[i] = (char*)malloc(sizeof(char) * size + 1)))
+		if (!(m.map[m.y] = (char*)malloc(sizeof(char) * m.size + 1)))
 			return (NULL);
-		j = 0;
-		while (j < size)
+		m.x = 0;
+		while (m.x < m.size)
 		{
-			map[i][j] = '.';
-			j++;
-			printf("map[i][j], i = %d, j = %d\n ", i, j);
+			m.map[m.y][m.x] = '.';
+			m.x++;
+			printf("map[m.y][m.x], m.y = %d, m.x = %d\n ", m.y, m.x);
 		}
-		map[i][j] = '\0';
-		i++;
-		printf("test_malloc_map_dans boucle, i = %d, j back zero = %d, (size + 1) = %d\n ", i, j, (size + 1));
+		m.map[m.y][m.x] = '\0';
+		m.y++;
+		printf("test_malloc_map_dans boucle, m.y = %d, m.x back zero = %d, (size + 1) = %d\n ", m.y, m.x, (m.size + 1));
 	}
-	map[i] = NULL;
-	printf("i_fin_malloc_map = %d, j = %d\n ", i, j);
-	return (map);
+	m.map[m.y] = NULL;
+	printf("m.y_fin_malloc_map = %d, m.x = %d\n ", m.y, m.x);
+	return (m.map);
 }
 
 void		remove_piece(char **map, int i)
@@ -89,11 +86,87 @@ void		remove_piece(char **map, int i)
 	}
 }
 
+int			can_place(char **tab, char **map)
+{
+
+	t_piece		t;
+	t_map		m;
+
+	map = m.map;
+	t.r = 0;
+	while (t.r < 4)
+	{
+		t.c = 0;
+		while (t.c < 4)
+		{
+			if (((tab[t.r][t.c] <= 'Z') && (tab[t.r][t.c] >= 'A')) && (map[t.r + m.y][t.c + m.x] != '.'))
+				return (-1);
+			else
+				t.c = t.c + 1;
+		}
+		t.r++;
+	}
+	return (0);
+}
+
+void		put_in_map(char **tab, char **map)
+{
+	t_piece		t;
+	t_map		m;
+
+	map = m.map;
+	t.r = -1;
+	while (t.r < 4)
+	{
+		t.c = -1;
+		while (t.c < 4)
+		{
+			if (((tab[t.r][t.c] <= 'Z') && (tab[t.r][t.c] >= 'A')) && (map[t.r + m.y][t.c + m.x] == '.'))
+				m.map[t.r + m.y][t.c + m.x] = tab[t.r][t.c];
+			else
+				t.c = t.c + 1;;
+		}
+		t.r++;
+	}
+}
+
+char		**backtrac(char ***tab)
+{
+	t_piece		p;
+	t_map		m;
+	p.w = count_tetris(tab);
+	m.size = count_tetris(tab);
+	m.map = malloc_map(tab, m.size);
+	print_tab(m.map);
+	m.x = 0;
+	m.y = 0;
+	while (count_tetris(tab) > 1)
+	{
+	printf("backtrac: i = %d, w = %d, size = %d\n ", p.i, p.w, m.size);
+		if (can_place(tab[p.i], m.map) == 0)
+		{
+	printf("backtrac2: i = %d, w = %d, size = %d\n ", p.i, p.w, m.size);
+			put_in_map(tab[p.i], m.map);
+			p.i++;
+			m.size--;
+		}
+		else if (can_place(tab[p.i], m.map) == -1 && m.x++ != '\0')
+			m.x++;
+		else if (can_place(tab[p.i], m.map) == -1 && m.x++ == '\0' && m.map[m.y++][0] != '\0')
+			{
+				m.x = 0;
+				m.y++;
+			}
+		}
+		return (m.map);
+}
+
+
 char		**check_map(char ***tab)
 {
 	char	**map = NULL;    // pr map
-	int		x_start;
-	int		y_start;
+//	int		x_start;
+//	int		y_start;
 	int		x;
 	int		y;
 	int		size;
@@ -103,8 +176,8 @@ char		**check_map(char ***tab)
 	int		c;
 	int		w;
 
-	x_start = 0;
-	y_start = 0;
+	x = 0;
+	y = 0;
 	w = count_tetris(tab);
 	size = count_tetris(tab);
 	map = malloc_map(tab, size);
@@ -112,14 +185,14 @@ char		**check_map(char ***tab)
 	i = 0;
 	r = 0;
 	c = 0;
+	x = 0;
+	y = 0;
 	printf("check_map_debut: i = %d, w = %d, size = %d\n ", i, w, size);
-	while (w > 0)
+	while (w > 1)
 	{
 //		r = 0;
-//	x_start = 0;
-//	y_start = 0;
-		printf("check_map_debut_while: i = %d, w = %d, size = %d, y_start = %d, x_start = %d\n ", i, w, size, y_start, x_start);
-		while (map[y_start][x_start] != '.' && map[x_start][y_start] != '\0')
+		printf("check_map_debut_while: i = %d, w = %d, size = %d, y = %d, x = %d\n ", i, w, size, y, x);
+/*		while (map[y_start][x_start] != '.' && map[x_start][y_start] != '\0')
 		{
 			printf("check_map_debut_while_2: y_start = %d, x_start =  %d\n ", y_start, x_start);
 			x_start++;
@@ -129,36 +202,41 @@ char		**check_map(char ***tab)
 				y_start++;
 			}
 			printf("check_map_fin_while_2: y_start = %d, x_start =  %d\n ", y_start, x_start);
-		}
+		}*/
 		r = 0;
 		printf("check_map_tab[i][r][c]: i = %d, r = %d, c = %d\n ", i, r, c);
+		while (map[y][x] != '.' && map[y][x] != '\0')
+		{
+			x++;
+			if (map[y][x] == '\0' && map[y + 1][0] != '\0')
+			{
+				x = 0;
+				y++;
+			}
+		}
+			printf("check_map_milieu_while_2: y = %d, x =  %d, c = %d, r = %d, i = %d, first_r(tab[i]) = %d, first_c(tab[i])= %d\n ", y, x, c, r, i, first_r(tab[i]), first_c(tab[i]));
+			fflush(stdout);
 		while (r < 4 && c < 4)
 		{
-			x = x_start;
-			y = y_start;
 			while (r < 4 && tab[i][r][c] != '\0')
 			{
-				if ((tab[i][r][c] != '.' && tab[i][r][c] != '\0') && (map[y][x] == '.' && map[y][x] != '\0'))
+				if ((tab[i][r][c] != '.' && tab[i][r][c] != '\0')
+						&& (map[y + r - first_r(tab[i])][x + c - first_c(tab[i])] == '.'))
 				{
-					map[y][x] = i + 65;
+					map[y + r - first_r(tab[i])][x + c - first_c(tab[i])] = i + 65;
 					c++;
 		//			let++;
-					if (tab[i][r][c + 1] == i + 65)
-						printf("entrela");x++;
 				}
-				else if (tab[i][r][c] == '.' && tab[i][r][c] != '\0')
-				{
+				else if (tab[i][r][c] == '.' && c < 4)
 					c++;
-//					x++;
-				}
 			printf("check_map_milieu_while_3: y = %d, x =  %d, c = %d, r = %d, i = %d\n ", y, x, c, r, i);
-				if (c == 3 && r < 4)
+				if (c == 4 && r < 4)
 				{
 					r++;
-					x = x_start;// -let -// first_i(tab[i], (i + 65));
+//					x--;
 					c = 0;
-					y++;
-			printf("check_map_milieu_change de rang: first_i(tab[i], (i + 65)) = %d, y = %d, x =  %d, c = %d, r = %d, i = %d, i + 65 = %c\n ",first_i(tab[i], (i + 65)),  y, x, c, r, i, (i + 65));
+//					y = 0;
+			printf("check_map_milieu_change de rang: first_c(tab[i]) = %d, y = %d, x =  %d, c = %d, r = %d, i = %d, i + 65 = %c\n ",first_c(tab[i]),  y, x, c, r, i, (i + 65));
 				}
 			printf("check_map_fin_while_3: y = %d, x =  %d, c = %d, r = %d, i = %d\n ", y, x, c, r, i);
 				print_tab(map);
@@ -174,7 +252,7 @@ char		**check_map(char ***tab)
 //		c = 0;
 		printf("sort de boucle\n");
 	}
-			if ((tab[i][r][c] != '.' && tab[i][r][c] != '\0') && (map[y][x] == '\0' || map[y][x] != '.')) //pb car i trop grand
+/*			if ((tab[i][r][c] != '.' && tab[i][r][c] != '\0') && (map[y][x] == '\0' || map[y][x] != '.')) //pb car i trop grand
 			{
 				remove_piece(map, i);
 				if (map[y][x_start + 1] != '\0')
@@ -193,7 +271,7 @@ char		**check_map(char ***tab)
 					size = size + 1;
 					map = malloc_map(tab, size);
 				}
-			}
+			}*/
 	return (map);
 }
 
@@ -218,6 +296,7 @@ int		main(int ac, char **av)
 		}
 	printf("test3, i = %d\n", i);
 	map = check_map(tab);
+	print_tab(map);
 	printf("test4, i = %d\n", i);
 	return (0);
 }

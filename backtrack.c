913a5886714b343afbf5ef031230dd05c6084ecc
Cracky-Kroll <ccarole@student.42.fr>
2019-07-20 18:35:54 +0200

    backtracking

--- backtrack.c
+++ backtrack.c
@@ -6,7 +6,7 @@
 /*   By: ccarole <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 21:36:17 by ccarole           #+#    #+#             */
-/*   Updated: 2019/07/17 12:34:21 by ccarole          ###   ########.fr       */
+/*   Updated: 2019/07/20 17:14:08 by ccarole          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,13 @@ char	**malloc_map(int size)
 {
 	t_map	m;
 
-//	nb_p = count_tetris(tab);
-//	m.size = ft_approx_sqrt(nb_p * 4);
 	m.y = 0;
 	m.x = 0;
-	printf("test_malloc_map_00, m.y = %d, size = %d\n", m.y, size);
+//	printf("test_malloc_map_00, m.y = %d, size = %d\n", m.y, size);
 	if (!(m.map = (char**)malloc(sizeof (char*) * size + 1)))
 		return (NULL);
-	printf("test_malloc_map, m.y = %d, size = %d\n", m.y, size);
-	fflush(stdout);
+//	printf("test_malloc_map, m.y = %d, size = %d\n", m.y, size);
+//	fflush(stdout);
 	while (m.y < size)
 	{
 		if (!(m.map[m.y] = (char*)malloc(sizeof(char) * size + 1)))
@@ -49,14 +47,15 @@ char	**malloc_map(int size)
 		{
 			m.map[m.y][m.x] = '.';
 			m.x++;
-			printf("map[m.y][m.x], m.y = %d, m.x = %d\n ", m.y, m.x);
+//			printf("map[m.y][m.x], m.y = %d, m.x = %d\n ", m.y, m.x);
 		}
 		m.map[m.y][m.x] = '\0';
 		m.y++;
-		printf("test_malloc_map_dans boucle, m.y = %d, m.x back zero = %d, (size + 1) = %d\n ", m.y, m.x, (size + 1));
+//		printf("test_malloc_map_dans boucle, m.y = %d, m.x back zero = %d, (size + 1) = %d\n ", m.y, m.x, (size + 1));
 	}
 	m.map[m.y] = NULL;
-	printf("m.y_fin_malloc_map = %d, m.x = %d\n ", m.y, m.x);
+//	printf("m.y_fin_malloc_map = %d, m.x = %d\n ", m.y, m.x);
+//	print_tab(m.map);
 	return (m.map);
 }
 
@@ -67,7 +66,7 @@ int			c_ref(char **tab)        // pour touver premier caractere dans le tableau 
 	c = 0;
 	while (tab[0][c] == '.' && tab[0][c] != '\0')
 		c++;
-	printf("c_ref = %d\n", c);
+//	printf("c_ref = %d\n", c);
 	return (c);
 }
 
@@ -82,53 +81,46 @@ int		check_map(char ***tab, char **map)
 	y = 0;
 	while (i < count_tetris(tab))
 	{
-		printf("check_map : debut while i = %d, count_tetris(tab) = %d\n", i, count_tetris(tab));
+//		printf("check_map : debut while i = %d, count_tetris(tab) = %d\n", i, count_tetris(tab));
 		if (can_place(map, tab[i], x, y) == 0)
 		{
-//			x = 0;
-//			y = 0;
-			printf("check_map AVANT PUT:: i = %d, x = %d, y = %d\n", i, x, y);
+//			printf("check_map AVANT PUT:: i = %d, x = %d, y = %d\n", i, x, y);
 			put_in_map(tab[i], map, x, y);
 			i++;
 			x = 0;
 			y = 0;
-			printf("check_map : APRES PUT: i = %d, x = %d, y = %d, count_tetris = %d\n", i, x, y, count_tetris(tab));
-//			if (i == count_tetris(tab)){
-//				printf("TEEEEEEEEEEEEESSSSSSTTT");	
-//				return (0);
-//			}
-//			print_tab(map);
+//			printf("check_map : APRES PUT: i = %d, x = %d, y = %d, count_tetris = %d\n", i, x, y, count_tetris(tab));
 		}
 		else if (x < len_map(map) - 1)
 		{
-			printf("CHECK : ELSE IF ; x = %d < len - 1 = %d\n", x, len_map(map) - 1);
+//			printf("CHECK : ELSE IF ; x = %d < len - 1 = %d\n", x, len_map(map) - 1);
 			x++;
 		}
 		else if (y < len_map(map) - 1)
 		{
-			printf("CHECK : ELSE IF ; y = %d < len - 1 = %d\n", y, len_map(map) - 1);
 			x = 0;
 			y++;
 		}
 		else
 		{
-			printf("CHECK MAP : ELSE  // i = %d\n", i);
 			i--;
 			if (i < 0)
 				return (-1);
-			printf("CHECK MAP // i A REMOVE = %d, x = %d, y = %d\n", i, x, y);
+//			printf("CHECK MAP // i A REMOVE = %d, x = %d, y = %d\n", i, x, y);
 			remove_piece(map, i, &x, &y);
-			printf("CHECK MAP APRES REMOVE / X = %d, Y = %d\n", x, y);
+//			printf("CHECK MAP APRES REMOVE / X = %d, Y = %d\n", x, y);
 		}
 	}
+
+	printf("sort check_map");
+//	print_tab(map);
+//	printf("sort check_map");
 	return (0);
 }
-
+/*								dans fichier main.c
 int		main(int ac, char **av)
 {
-	printf("ENTRE DANS MAIN");
 	char	***tab;
-//	int		i = 0;
 	char	**map;
 	int		size;
 
@@ -140,6 +132,7 @@ int		main(int ac, char **av)
 	printf("MAIN ; MAP APRES 1ER MALLOC\n");
 	print_tab(map);
 	printf("MAIN :: test1, ac = %d\n", ac);
+	//faire fonction d'erreur
 	if (tab == NULL)                       // free ici ou plus tot ?
 		printf("Piece invalide :( %s%d\n", __FILE__, __LINE__);
 	printf("MAIN :: test3, \n");
@@ -158,4 +151,4 @@ int		main(int ac, char **av)
 //	free(tab);
 //	free(map);
 	return (0);
-}
+}*/
